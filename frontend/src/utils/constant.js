5013c625c4596ce09d83783c9d381802ca5cba6e
md irshad <m.irshad51068@gmail.com>
2025-08-14 12:10:50 +0530

    final commit

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,3 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const Product_API_END_POINT="http://localhost:8000/api/v1/product";
-export const Cart_API_END_POINT="http://localhost:8000/api/v1/cart";
+export const USER_API_END_POINT="https://blinkit-ecom-xmcl.onrender.com/api/v1/user";
+export const Product_API_END_POINT="https://blinkit-ecom-xmcl.onrender.com/api/v1/product";
+export const Cart_API_END_POINT="https://blinkit-ecom-xmcl.onrender.com/api/v1/cart";
